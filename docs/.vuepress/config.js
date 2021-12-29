acde1a064131f990bc28cf9661bfc4e1b62c8bf0
ub3rb3457 <perchance2dream86@gmail.com>
2021-12-29 18:25:43 -0500

    edit config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,34 +67,9 @@ module.exports = {
     sidebar: {
       '/note/': [
         {
-          title: 'HTML5',
-          collapsable: true,
-          children: ['html5/HTML5的语义元素', 'html5/HTML5多媒体标签', 'html5/HTML5表单元素', 'html5/HTML5中的API', 'html5/Canvas']
-        },
-        {
           title: 'CSS',
           collapsable: true,
-          children: ['css/css3新特性', 'css/css3Flip']
-        },
-        {
-          title: 'JS',
-          collapsable: true,
-          children: ['js/js数据类型', 'js/js对象', 'js/js继承', 'js/js原型链、闭包', 'js/js函数的四种调用方式']
-        },
-        {
-          title: 'Vue.js',
-          collapsable: true,
-          children: ['Vue/Vue基础笔记', 'Vue/Vue组件']
-        },
-        {
-          title: '前端单元测试',
-          collapsable: true,
-          children: ['fe-unit-test/chai', 'fe-unit-test/mocha', 'fe-unit-test/vueTestUtils']
-        },
-        {
-          title: '微信小程序',
-          collapsable: true,
-          children: ['wechat-mini-program/初识微信小程序']
+          children: ['css/css3Flip']
         }
       ]
     },
